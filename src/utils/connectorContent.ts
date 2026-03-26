33e7d580f000c745348efd4b98e8404eac818bf1
ProfSynapse <jrosenbaum9689@gmail.com>
2026-03-26 17:11:56 -0400

    chore: bump version to 5.5.2

--- src/utils/connectorContent.ts
+++ src/utils/connectorContent.ts
@@ -5,7 +5,7 @@
  * DO NOT EDIT MANUALLY - This file is regenerated during the build process.
  * To update, modify connector.ts and rebuild.
  *
- * Generated: 2026-03-26T21:07:11.321Z
+ * Generated: 2026-03-26T21:11:50.375Z
  */
 
 export const CONNECTOR_JS_CONTENT = `"use strict";
